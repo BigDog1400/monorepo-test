d057508278a3524eb379ffae8d48ac27b4558b7b
Isaac Peña <isaac.pr99@gmail.com>
2022-11-03 12:53:24 -0400

    feat: update react app example with chakra styles

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useState } from "react";
+import { add } from "math-helpers";
+import { Box, Button, ButtonGroup } from "@chakra-ui/react";
+import { Card, Pagination } from "styled-ui";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React app</h1>
+      <ButtonGroup>
+        <Button onClick={() => alert(add(1, 2))}>Sumar 1 + 2</Button>
+        <Button variant={"brandPrimary"}>Chakra Button</Button>
+      </ButtonGroup>
+      <Card />
+      <Box
+        bg="#edf3f8"
+        _dark={{
+          bg: "#3e3e3e",
+        }}
+        w="fit-content"
+        p={5}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Pagination />
+      </Box>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- apps/react-app/src/main.tsx
+++ apps/react-app/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { customTheme } from "styled-ui/src";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
-)
+);
