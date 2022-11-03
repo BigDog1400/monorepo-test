0dfabc067962ba7d143d1584c3f515081e52d812
Isaac Peña <isaac.pr99@gmail.com>
2022-11-02 20:59:30 -0400

    feat: add new internal package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    transpilePackages: ["@bigdog1400/ui"],
+    transpilePackages: ["@bigdog1400/ui", "math-helpers"],
   },
 };
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@bigdog1400/ui";
+import { add } from "math-helpers";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <button onClick={() => alert(add(1, 2))}>Sumar 1 + 2</button>
       <Button />
     </div>
   );
