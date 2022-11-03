ef5d155e02d0594c7db9970d3b67fa75537ad6ca
Isaac Peña <isaac.pr99@gmail.com>
2022-11-03 14:17:24 -0400

    feaT: add comment to check tree shaking

--- packages/math-helpers/src/index.ts
+++ packages/math-helpers/src/index.ts
@@ -1,7 +1,9 @@
 export const add = (a: number, b: number) => {
+  // this is a comment
   return a + b;
 };
 
 export const subtract = (a: number, b: number) => {
+  // this is also a  comment
   return a - b;
 };
